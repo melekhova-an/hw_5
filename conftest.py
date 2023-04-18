9d4c4605abae6f37c103f436f49485146cf71464
Anastasiya Melekhova <amelekhova-aan@yandex.ru>
2023-04-18 13:26:50 +0300

    dz fix commit

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_url():
-    browser.config.base_url = ('https://demoqa.com')
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 1200
     browser.config.window_width = 1500
 
