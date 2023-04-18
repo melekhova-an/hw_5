0e4c27df04401ff8201e86f5c82f8957433ed003
Anastasiya Melekhova <amelekhova-aan@yandex.ru>
2023-04-18 11:48:41 +0300

    dz fix commit

--- hw_5.py
+++ hw_5.py
@@ -1,21 +1,22 @@
-from selene import browser, have, be
+from selene import browser, have
+import os
 
 
 def test_complete_the_form():
-    browser.open_url('/automation-practice-form')
+    browser.open('/automation-practice-form')
 
     # Ввод имени и фамилии
     browser.element('#firstName').type('An')
     browser.element('#lastName').type('Melekhova')
 
     # Ввод почты
-    browser.element('#userEmail').type('melekhova.ai@gmail.com')
+    browser.element('#userEmail').type('test@gmail.com')
 
     # Выбор пола
     browser.element('#gender-radio-2').double_click()
 
     # Ввод номера
-    browser.element('#userNumber').type('89637569569')
+    browser.element('#userNumber').type('89000500500')
 
     # Выбор даты рождения
     browser.element('#dateOfBirthInput').press()
@@ -29,11 +30,11 @@ def test_complete_the_form():
     browser.element('#subjectsInput').type('Hindi').press_enter()
 
     # Выбор хобби
-    browser.element('#hobbies-checkbox-1').double_click()
-    browser.element('#hobbies-checkbox-3').double_click()
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+    browser.element('label[for="hobbies-checkbox-3"]').click()
 
     # Загрузка файла
-    browser.element('#uploadPicture').send_keys('/Users/anastasiyamelekhova/dev/qaguru/IMAGE.jpg')
+    browser.element('#uploadPicture').send_keys(os.getcwd()+'/IMAGE.jpg')
 
     # Заполнение адреса
     browser.element('#currentAddress').type('Moscow, b1')
@@ -45,3 +46,6 @@ def test_complete_the_form():
 
     # Проверка - форма отправлена
     browser.element('[class="modal-title h4"]').should(have.exact_text('Thanks for submitting the form'))
+    browser.all('tbody tr').should(have.exact_texts('Student Name An Melekhova', 'Student Email test@gmail.com',
+    'Gender Female', 'Mobile 8900050050', 'Date of Birth 09 April,1997', 'Subjects Hindi', 'Hobbies Sports, Music',
+    'Picture IMAGE.jpg', 'Address Moscow, b1', 'State and City Uttar Pradesh Agra'))
